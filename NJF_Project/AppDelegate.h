42b635a001ca51008b864412d40d929433dff19f
jinfeng niu <861464752@qq.com>
2018-09-11 23:58:16 +0800

    初始化提交

---
+++ NJF_Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NJF_Project
+//
+//  Created by jinfeng niu on 2018/9/11.
+//  Copyright © 2018年 jinfeng niu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NJF_ProjectTests/NJF_ProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  NJF_ProjectTests.m
+//  NJF_ProjectTests
+//
+//  Created by jinfeng niu on 2018/9/11.
+//  Copyright © 2018年 jinfeng niu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NJF_ProjectTests : XCTestCase
+
+@end
+
+@implementation NJF_ProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
