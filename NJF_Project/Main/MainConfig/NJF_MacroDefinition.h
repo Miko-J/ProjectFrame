8e9b1e1689996f81b8bf8e252cd4021d9c95ff93
niujf <niujf@kingnet.com>
2019-01-24 18:05:42 +0800

    添加宏定义

--- NJF_Project/Main/MainConfig/NJF_MacroDefinition.h
+++ NJF_Project/Main/MainConfig/NJF_MacroDefinition.h
@@ -9,7 +9,85 @@
 #ifndef NJF_MacroDefinition_h
 #define NJF_MacroDefinition_h
 
-#define NJF_Color(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+/************************Application相关*******************/
+
+#define APPLICATION         [UIApplication sharedApplication]
+#define APPDLE              (AppDelegate*)[APPLICATION delegate]
+#define kKeyWindow          [UIApplication sharedApplication].keyWindow
+#define kUserDefaults       [NSUserDefaults standardUserDefaults]
+#define kNotificationCenter [NSNotificationCenter defaultCenter]
+//获取temp
+#define kPathTemp           NSTemporaryDirectory()
+//获取沙盒 Document
+#define kPathDocument       [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject]
+//获取沙盒 Cache
+#define kPathCache          [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject]
+
+/************************屏幕坐标、尺寸相关*******************/
+
+//判断是否是ipad
+#define isPad ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+//判断iPhone4系列
+#define kiPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhone5系列
+#define kiPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhone6 6s 7系列
+#define kiPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhone6p 6sp 7p系列
+#define kiPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhoneX，Xs（iPhoneX，iPhoneXs）
+#define IS_IPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhoneXr
+#define IS_IPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhoneXsMax
+#define IS_IPHONE_Xs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size)&& !isPad : NO)
+//判断iPhoneX所有系列
+#define IS_PhoneXAll (IS_IPHONE_X || IS_IPHONE_Xr || IS_IPHONE_Xs_Max)
+// 屏幕高度
+#define kScreenHeight           [[UIScreen mainScreen] bounds].size.height
+// 屏幕宽度
+#define kScreenWidth            [[UIScreen mainScreen] bounds].size.width
+// 顶部导航栏高度
+#define kNavigationBarHeight 44.0f
+// 状态栏高度
+#define kStatusBarHeight (IS_PhoneXAll? 44.0 : 20.0)
+// 顶部安全距离
+#define kSafeAreaTopHeight (IS_PhoneXAll ? 88.0 : 64.0)
+// 底部安全距离
+#define kSafeAreaBottomHeight (IS_PhoneXAll ? 34.f : 0.f)
+// Tabbar高度
+#define kTabbarHeight 49.f
+// 去除上下导航栏剩余中间视图高度
+#define ContentHeight   (kScreenHeight - kSafeAreaTopHeight - kSafeAreaBottomHeight -kTabbarHeight)
+
+#pragma mark - 系统相关
+
+/************************app版本号相关*******************/
+
+#define DEVICE_APP_VERSION      (NSString *)[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
+// app Build版本号
+#define DEVICE_APP_BUILD        (NSString *)[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]
+// 系统版本号（string）
+#define DEVICE_OS_VERSION       [[UIDevice currentDevice] systemVersion]
+// 系统版本号（float）
+#define DEVICE_OS_VERSION_VALUE [DEVICE_OS_VERSION floatValue]
+
+/************************字体、颜色相关*******************/
+
+#define FONT_SIZE(f)            [UIFont systemFontOfSize:(f)]
+#define FONT_BOLD_SIZE(f)       [UIFont boldSystemFontOfSize:(f)]
+#define FONT_ITALIC_SIZE(f)     [UIFont italicSystemFontOfSize:(f)]
+#define RGBCOLOR(r,g,b)         [UIColor colorWithRed:(r)/255.f green:(g)/255.f blue:(b)/255.f alpha:1.f]
+#define RGBACOLOR(r,g,b,a)      [UIColor colorWithRed:(r)/255.f green:(g)/255.f blue:(b)/255.f alpha:(a)]
+#define RandomColor             [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
+#define ColorWithHex(rgbValue)  [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0]
+
+/************************判断数据是否为空*******************/
+
+#define kStringIsEmpty(str)     ([str isKindOfClass:[NSNull class]] || str == nil || [str length] < 1 ? YES : NO )
+#define kArrayIsEmpty(array)    (array == nil || [array isKindOfClass:[NSNull class]] || array.count == 0)
+#define kDictIsEmpty(dic)       (dic == nil || [dic isKindOfClass:[NSNull class]] || dic.allKeys == 0)
+
 
 
 #endif /* NJF_MacroDefinition_h */
